1e59a08d9e634a3beef2fda0f74304213492fc62
mdnisar-ahmed <mdnisar.ahmed@hotmail.com>
2025-06-03 20:21:25 -0500

    Changes in MOQHelper Class

--- gWorks.Hiring.TestConsoleApplication.Tests/MockHelpers.cs
+++ gWorks.Hiring.TestConsoleApplication.Tests/MockHelpers.cs
@@ -8,6 +8,28 @@ internal static class MockHelpers
     internal static (Mock<IRepository<T>> Mock, ICollection<T> DataSet) CreateMockRepo<T>()
         where T : class
     {
+        /* var dataSet = new List<T>();
+
+         var mock = new Mock<IRepository<T>>();
+         mock.Setup(x => x.AddRange(It.IsAny<IEnumerable<T>>())).Callback<IEnumerable<T>>(dataSet.AddRange).Verifiable();
+         mock.Setup(x => x.Add(It.IsAny<T[]>())).Callback<T[]>(dataSet.AddRange).Verifiable();
+         mock.Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<T>>())).Returns<IEnumerable<T>>(items => { dataSet.AddRange(items); return Task.CompletedTask; }).Verifiable();
+         mock.Setup(x => x.AddAsync(It.IsAny<T[]>())).Returns<T[]>(items => { dataSet.AddRange(items); return Task.CompletedTask; }).Verifiable();
+
+         mock.Setup(x => x.RemoveRange(It.IsAny<IEnumerable<T>>())).Callback<IEnumerable<T>>(items =>
+         {
+             foreach (var item in items.ToList())
+                 dataSet.Remove(item);
+         }).Verifiable();
+         mock.Setup(x => x.Remove(It.IsAny<T[]>()))
+     .Callback<T[]>(items =>
+     {
+         foreach (var item in items)
+             dataSet.Remove(item);
+     }).Verifiable();
+
+         mock.SetupGet(x => x.Query).Returns(() => dataSet.AsQueryable()).Verifiable();
+         return (mock, dataSet);*/
         var dataSet = new List<T>();
 
         var mock = new Mock<IRepository<T>>();
@@ -16,8 +38,17 @@ internal static class MockHelpers
         mock.Setup(x => x.AddRangeAsync(It.IsAny<IEnumerable<T>>())).Returns<IEnumerable<T>>(items => { dataSet.AddRange(items); return Task.CompletedTask; }).Verifiable();
         mock.Setup(x => x.AddAsync(It.IsAny<T[]>())).Returns<T[]>(items => { dataSet.AddRange(items); return Task.CompletedTask; }).Verifiable();
 
-        mock.Setup(x => x.RemoveRange(It.IsAny<IEnumerable<T>>())).Callback(dataSet.RemoveRange).Verifiable();
-        mock.Setup(x => x.Remove(It.IsAny<T[]>())).Callback(dataSet.RemoveRange).Verifiable();
+        mock.Setup(x => x.RemoveRange(It.IsAny<IEnumerable<T>>())).Callback<IEnumerable<T>>(items =>
+        {
+            foreach (var item in items.ToList())
+                dataSet.Remove(item);
+        }).Verifiable();
+        mock.Setup(x => x.Remove(It.IsAny<T[]>()))
+    .Callback<T[]>(items =>
+    {
+        foreach (var item in items)
+            dataSet.Remove(item);
+    }).Verifiable();
 
         mock.SetupGet(x => x.Query).Returns(() => dataSet.AsQueryable()).Verifiable();
         return (mock, dataSet);
